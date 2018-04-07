50e80da8c89ca5db9d97bc3f11ed44b55ea44f8a
SamiJG <samantha.gildart@gmail.com>
2018-04-07 19:39:22 +0100

    add comment count on getAllArticlesForTopic

--- controllers/topics.js
+++ controllers/topics.js
@@ -1,4 +1,4 @@
-const { Topic, Article } = require('../models');
+const { Topic, Article, Comment } = require('../models');
 
 function getAllTopics(req, res, next) {
   Topic.find()
@@ -8,6 +8,28 @@ function getAllTopics(req, res, next) {
 
 function getAllArticlesForTopic(req, res, next) {
   Article.find({ topic: req.params.topic })
+    .populate('created_by', 'username')
+    .then(articles => {
+      return Promise.all([articles, Comment.find()]);
+    })
+    .then(([articleDocs, comments]) => {
+      const commentCount = comments.reduce((acc, comment) => {
+        const username = comment.belongs_to;
+        acc[username] ? acc[username]++ : (acc[username] = 1);
+        return acc;
+      }, {});
+      return articleDocs.map(article => {
+        return {
+          title: article.title,
+          body: article.body,
+          topic: article.topic,
+          created_by: article.created_by.username,
+          votes: article.votes,
+          comments: commentCount[article._id],
+          _id: article._id
+        };
+      });
+    })
     .then(articles => res.send({ articles }))
     .catch(next);
 }
